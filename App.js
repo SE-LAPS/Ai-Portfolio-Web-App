87de5fb553281c097b08e836383226f1366aa053
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-01-16 13:05:39 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,36 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Preloader from "../src/components/Pre";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Projects from "./components/Projects/Projects";
+import Footer from "./components/Footer";
+import Resume from "./components/Resume/ResumeNew";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate
+} from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
+import "./style.css";
 import "./App.css";
-
-// import Router
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
-// import Component
-import Header from "./component/Header";
-import Footer from "./component/Footer";
-import ScrollToTop from "./component/ScrollToTop";
-
-// import Pages
-import Home from "./Pages/Home";
-import ReactHooks from "./Pages/ReactHooks";
-import UiUx from "./Pages/UiUx";
-import FormExample from "./Pages/FormExample";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const [load, upadateLoad] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      upadateLoad(false);
+    }, 1200);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <React.StrictMode>
-      <Router>
-        <Header />
-        <ScrollToTop>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/react-hooks" component={ReactHooks} />
-            <Route exact path="/ui-ux" component={UiUx} />
-            <Route exact path="/form-example" component={FormExample} />
-          </Switch>
-        </ScrollToTop>
+    <Router>
+      <Preloader load={load} />
+      <div className="App" id={load ? "no-scroll" : "scroll"}>
+        <Navbar />
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/project" element={<Projects />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="*" element={<Navigate to="/"/>} />
+        </Routes>
         <Footer />
-      </Router>
-    </React.StrictMode>
+      </div>
+    </Router>
   );
 }
 
