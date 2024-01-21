30681ce47bd543e39904ee8234ef53c409e2163f
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-01-22 00:22:13 +0530

    Update index.js

--- components/index.js
+++ components/index.js
@@ -1,72 +1,63 @@
-import React, { useState } from "react";
+import React from "react";
 import "./style.css";
-import { VscGrabber, VscClose } from "react-icons/vsc";
-import { Link } from "react-router-dom";
-import { logotext ,socialprofils } from "../content_option";
-import Themetoggle from "../components/themetoggle";
+import {
+  FaGithub,
+  FaTwitter,
+  FaFacebookF,
+  FaLinkedin,
+  FaYoutube,
+  FaTwitch,
+} from "react-icons/fa";
+import { socialprofils } from "../../content_option";
 
-const Headermain = () => {
-  const [isActive, setActive] = useState("false");
-
-  const handleToggle = () => {
-    setActive(!isActive);
-    document.body.classList.toggle("ovhidden");
-  };
-
+export const Socialicons = (params) => {
   return (
-    <>
-      <header className="fixed-top site__header">
-        <div className="d-flex align-items-center justify-content-between">
-          <Link  className="navbar-brand nav_ac" to="/">
-            {logotext}
-          </Link>
-          <div className="d-flex align-items-center">
-          <Themetoggle />
-          <button className="menu__button  nav_ac" onClick={handleToggle}>
-            {!isActive ? <VscClose /> : <VscGrabber />}
-          </button>
-          
-          </div>
-        </div>
-
-        <div className={`site__navigation ${!isActive ? "menu__opend" : ""}`}>
-          <div className="bg__menu h-100">
-            <div className="menu__wrapper">
-              <div className="menu__container p-3">
-                <ul className="the_menu">
-                  <li className="menu_item ">
-                  <Link  onClick={handleToggle} to="/" className="my-3">Home</Link>
-                  </li>
-                  <li className="menu_item">
-                    <Link  onClick={handleToggle} to="/portfolio" className="my-3"> Portfolio</Link>
-                  </li>
-                  <li className="menu_item">
-                  <Link onClick={handleToggle} to="/about" className="my-3">About</Link>
-                  </li>
-                  <li className="menu_item">
-                  <Link onClick={handleToggle} to="/contact" className="my-3"> Contact</Link>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div className="menu_footer d-flex flex-column flex-md-row justify-content-between align-items-md-center position-absolute w-100 p-3">
-            <div className="d-flex">
-            <a href={socialprofils.facebook}>Facebook</a>
-            <a href={socialprofils.github}>Github</a>
-            <a href={socialprofils.twitter}>Twitter</a>
-            </div>
-            <p className="copyright m-0">copyright __ {logotext}</p>
-          </div>
-        </div>
-      </header>
-      <div className="br-top"></div>
-      <div className="br-bottom"></div>
-      <div className="br-left"></div>
-      <div className="br-right"></div>
-      
-    </>
+    <div className="stick_follow_icon">
+      <ul>
+        {socialprofils.twitter && (
+          <li>
+            <a href={socialprofils.twitter}>
+              <FaTwitter />
+            </a>
+          </li>
+        )}
+        {socialprofils.github && (
+          <li>
+            <a href={socialprofils.github}>
+              <FaGithub />
+            </a>
+          </li>
+        )}
+        {socialprofils.facebook && (
+          <li>
+            <a href={socialprofils.facebook}>
+              <FaFacebookF />
+            </a>
+          </li>
+        )}
+        {socialprofils.linkedin && (
+          <li>
+            <a href={socialprofils.linkedin}>
+              <FaLinkedin />
+            </a>
+          </li>
+        )}
+        {socialprofils.youtube && (
+          <li>
+            <a href={socialprofils.youtube}>
+              <FaYoutube />
+            </a>
+          </li>
+        )}
+        {socialprofils.twitch && (
+          <li>
+            <a href={socialprofils.twitch}>
+              <FaTwitch />
+            </a>
+          </li>
+        )}
+      </ul>
+      <p>Follow Me</p>
+    </div>
   );
 };
-
-export default Headermain;
